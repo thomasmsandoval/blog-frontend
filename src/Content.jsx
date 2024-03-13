36a9bbd0a24d3898064ca97eecd32684ca269950
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-13 13:59:21 -0700

    uses useEffect to load data one time only

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,22 +1,25 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { PostsIndex } from "./PostsIndex";
 import { PostsNew } from "./PostsNew";
 
 export function Content() {
   let name = "Thomas Sandoval";
-  let posts = [];
+  const [posts, setPosts] = useState([]);
 
   const handleIndexPosts = () => {
     axios.get("http://localhost:3000/posts.json").then((response) => {
-      posts = response.data;
+      setPosts(response.data);
       console.log(posts);
     });
   };
 
+  useEffect(handleIndexPosts, []);
+
   return (
     <main>
       <PostsNew />
-      <button onClick={handleIndexPosts}>Load Posts</button>
+      {/* <button onClick={handleIndexPosts}>Load Posts</button> */}
       <PostsIndex name={name} posts={posts} />
     </main>
   );

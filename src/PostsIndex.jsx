401f8a4baddf969a3892c141dfa82f984a928acd
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-13 09:45:58 -0700

    added more info button

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -8,6 +8,7 @@ export function PostsIndex(props) {
           <h2>{post.title}</h2>
           <img src={post.image} alt="" />
           <p>Body:{post.body}</p>
+          <button>More info</button>
         </div>
       ))}
     </div>

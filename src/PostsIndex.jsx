13077da0d77d54d9ca25aca55097313b12afa102
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-13 09:32:20 -0700

    separates components into files

---
+++ src/PostsIndex.jsx
@@ -0,0 +1,15 @@
+export function PostsIndex(props) {
+  console.log(props);
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      {props.posts.map((post) => (
+        <div key={post.id} className="posts">
+          <h2>{post.title}</h2>
+          <img src={post.image} alt="" />
+          <p>Body:{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/PostsNew.jsx
@@ -0,0 +1,21 @@
+export function PostsNew(props) {
+  console.log("The props are", props);
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <p>Blog by: {props.name}</p>
+      <form>
+        <div>
+          Title: <input type="text" />
+        </div>
+        <div>
+          Body: <input type="text" />
+        </div>
+        <div>
+          Image: <input type="url" />
+        </div>
+        <button type="submit">Create blog</button>
+      </form>
+    </div>
+  );
+}

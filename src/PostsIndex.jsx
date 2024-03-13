54e85caeea6a5a5900dfe5ca88aa6bb5a38b2989
thomasmsandoval <156626315+thomasmsandoval@users.noreply.github.com>
2024-03-13 10:54:37 -0700

    styles cards in grids

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -3,14 +3,16 @@ export function PostsIndex(props) {
   return (
     <div id="posts-index">
       <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="posts">
-          <h2>{post.title}</h2>
-          <img src={post.image} alt="" />
-          <p>Body:{post.body}</p>
-          <button>More info</button>
-        </div>
-      ))}
+      <div className="cards">
+        {props.posts.map((post) => (
+          <div key={post.id} className="posts">
+            <h2>{post.title}</h2>
+            <img src={post.image} alt="" />
+            <p>Body:{post.body}</p>
+            <button>More info</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
